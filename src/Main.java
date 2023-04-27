a40b569704d5f670cad16874d3e901bac20969e1
Marina Danilov <mdanilov2012@gmail.com>
2023-04-27 22:00:37 +0200

    change code style

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Marina!");
-        System.out.println("How are you?");
+        System.out.println("Change Code Style -> Google Style");
     }
 }
